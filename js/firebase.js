76d9bef9112b98c8dde61030bf383462af378c2c
evanfrawley <evanfrawley@gmail.com>
2016-02-27 22:00:14 -0800

    got firebase to work with the js and return properly

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,19 @@
 (function() {
+	"use strict";
 	//var Firebase = require("firebase");
+
+	window.onload = function() {
+
+	};
+
 	var myFirebaseRef = new Firebase("https://luminous-fire-9933.firebaseio.com");
-	myFirebaseRef.child("locations/latitude").set("oppai");
+
+	myFirebaseRef.child("locations/latitude").set("chinchin");
+
+	myFirebaseRef.on("value", function(snapshot) {
+		var chin = snapshot.child("locations/latitude");
+		console.log(chin.val());
+	});
+
+
 })();
--- r/wrangle.R
+++ r/wrangle.R
@@ -3,8 +3,15 @@
 library(dplyr)
 library(leaflet)
 library(jsonlite)
-library(shiny)
+library(knitr)
+
 
 setwd("C:/Users/Evan/Github/nwhacks2016/")
+
 data <- read.csv("data/Computer_Media_Center.csv")
 
+m <- leaflet() %>% 
+  addTiles() %>% 
+  addMarkers(lng = data$Longitude, lat = data$Latitude, popup = data$Address)
+
+m
