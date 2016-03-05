f47f25e289c2fa78360b0db327bf0d9c338ade78
evanfrawley <evanfrawley@gmail.com>
2016-03-05 15:10:10 -0800

    partitioning js files

--- js/main.js
+++ js/main.js
@@ -4,9 +4,7 @@
 (function() {
     "use strict";
 
-    //var map = L.map('map');
     window.onload = function () {
-        //loadMap();
         injectEmployers();
         //initCoursera();
     };
@@ -19,59 +17,17 @@
         var req = new XMLHttpRequest();
         req.onload = func;
         req.open("GET", url, true);
-        
         req.send();
-    }
-
-    /*
-     *    LEAFTLET JS
-     */
-    function loadMap() {
-
-        var key = "pk.eyJ1IjoiZXZhbmZyYXdsZXkiLCJhIjoiY2lqemV0cDJpMmx2a3Z3bTV2dGh1bmt0MSJ9.gJsWsiu3AareD8XkI1-0Aw";
-
-        var url = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=" + key;
-
-        L.tileLayer(url, {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            id: 'mapbox.streets'
-        }).addTo(map);
-
-        map.setView([47.6550, -122.3080], 13);
-        getData();
-    }
-
-    function getData() {
-        var url = "https://data.seattle.gov/resource/awnr-r8xe.json";
-        ajax(url, addMap);
-    }
-
-    function addMap() {
-        var data = JSON.parse(this.responseText);
-        //console.log(this.responseText);
-        for (var i = 0; i < data.length; i++) {
-            var popup = "<h3>" + data[i].address + "</h3>";
-            if (data[i].website !== undefined) {
-                popup += "<br /><a href=\"" + data[i].website.url + "\">Website</a>";
-            }
-            L.marker([data[i].latitude, data[i].longitude]).addTo(map).bindPopup(popup);
-
-        }
     }
 
 
     /*
      *      FIREBASE JS
      */
-
+    //var Firebase = require("firebase");
     var myFirebaseRef = new Firebase("https://luminous-fire-9933.firebaseio.com");
 
     myFirebaseRef.child("locations/latitude").set("chinchin");
-
-
 
     /*
      *      COURSERA JS
@@ -84,8 +40,6 @@
 
 
     }
-
-
 
     /*
      *     inject the employers
@@ -109,5 +63,6 @@
 
         });
     }
+
 
 })();
